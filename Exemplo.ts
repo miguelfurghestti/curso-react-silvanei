581d91dbdb5fb8f3c210b23e50e12ab7378dc066
Miguel Sampaio Furghestti <miguel.aghei@hotmail.com>
2023-12-20 16:45:30 -0300

    First commit

---
+++ Exemplo.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
+import { getRepository, Repository} from 'typeorm'
+
+interface  {
+
+}
+
+class exemplo.ts {
+
+}
+
+export { exemplo.ts }
