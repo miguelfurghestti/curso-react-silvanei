c417833f3e8ca43bc2beceb9b61a07011ebd3c49
Miguel Sampaio Furghestti <miguel.aghei@hotmail.com>
2023-12-20 22:54:02 -0300

    Deleted duped files and more...

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,26 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View
+    className="flex-1 items-center justify-center bg-slate-900
+  "
+    >
       <Text
-      style={{
-        fontSize: 30,
-        color: 'black',
-        fontWeight: 'bold',
-      }}
-      >Miguel Furghestti</Text>
-      <StatusBar style="auto" />
+      className="color-white text-2xl;">Miguel Furghestti</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
